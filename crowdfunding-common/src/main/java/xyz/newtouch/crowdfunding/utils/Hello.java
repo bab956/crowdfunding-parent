d8a96eb977e9d0e0a54b6933032cd49de6706860
weibgg <weibgg@163.com>
2020-10-26 00:51:59 +0800

    add

--- crowdfunding-common/src/main/java/xyz/newtouch/crowdfunding/utils/Hello.java
+++ crowdfunding-common/src/main/java/xyz/newtouch/crowdfunding/utils/Hello.java
@@ -1,7 +1,10 @@
 package xyz.newtouch.crowdfunding.utils;
 
+/**
+ * @author weibing
+ */
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
+        System.out.println("HelloWorld777");
     }
 }
