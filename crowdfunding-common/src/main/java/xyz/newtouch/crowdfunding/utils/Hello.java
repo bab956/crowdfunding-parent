760f594d32f0969449b8ecaa7174ba6b67a0217f
weibgg <weibgg@163.com>
2020-10-26 00:22:44 +0800

    update code

--- crowdfunding-common/src/main/java/xyz/newtouch/crowdfunding/utils/Hello.java
+++ crowdfunding-common/src/main/java/xyz/newtouch/crowdfunding/utils/Hello.java
@@ -5,6 +5,6 @@ package xyz.newtouch.crowdfunding.utils;
  */
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
+        System.out.println("HelloWorld666");
     }
 }
