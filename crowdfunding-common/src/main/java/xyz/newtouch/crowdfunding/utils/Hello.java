28f4a11d1d8bc5151b19f74485c70abbc789ef7a
weibgg <weibgg@163.com>
2020-10-26 00:42:05 +0800

    ADD CODE

---
+++ crowdfunding-common/src/main/java/xyz/newtouch/crowdfunding/utils/Hello.java
@@ -0,0 +1,11 @@
+package xyz.newtouch.crowdfunding.utils;
+
+/**
+ * @author weibing
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
+
