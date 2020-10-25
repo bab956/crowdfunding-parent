2ec51eda514b042fb8010177e841dff3bbfbc1be
weibgg <weibgg@163.com>
2020-10-26 00:45:26 +0800

    ADD CODE

--- crowdfunding-common/src/main/java/xyz/newtouch/crowdfunding/utils/Hello.java
+++ crowdfunding-common/src/main/java/xyz/newtouch/crowdfunding/utils/Hello.java
@@ -5,6 +5,6 @@ package xyz.newtouch.crowdfunding.utils;
  */
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("HelloWorld777");
+        System.out.println("HelloWorld7771");
     }
 }
